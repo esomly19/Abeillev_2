a21668bdd7eb069ff61da0ad89c75d05a5a96c89
legrand82u <theo.legrand7@etu.univ-lorraine.fr>
2020-01-30 08:53:33 +0100

    Changement du JS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import QrScanner from './qr-scanner.min.js';
 
 QrScanner.WORKER_PATH = './qr-scanner-worker.min.js';
 // Set constraints for the video stream
-var constraints = {video: {facingMode: "environment"}, audio: false};// Define constants
+const constraints = {video: {facingMode: "environment"}, audio: false};// Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
@@ -18,20 +18,20 @@ function cameraStart() {
         .catch(function (error) {
             console.error("Oops. Something is broken.", error);
         });
-}// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function () {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-    QrScanner.scanImage(document.getElementById("camera--output"))
-        .then(result => alert("QR Code : " + result))
-        .catch(err => alert(err))
-};// Start the video stream when the window loads
+}
+
 window.addEventListener("load", () => {
     cameraStart();
 
-    const qrScanner = new QrScanner(document.getElementById("camera--view"), result => alert("QR Code = " + result));
+    const qrScanner = new QrScanner(document.getElementById("camera--view"), result => {
+        qrScanner.destroy();
+        fetch('http://localhost:19080/fleurs/'+result).then((result) => {
+            result.json().then((result) => {
+                console.log(result[0].n_fr);
+
+                // window.location.href="http://51.158.67.211:19080/fleurs/"+result[0].id;
+            })
+        });
+    });
     qrScanner.start();
 }, false);
