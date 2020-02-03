28243c27b532b501f3b8dc85c9fe605a83e03e87
Thomas <esomly57@gmail.com>
2020-02-03 21:18:39 +0100

    pseudo work

--- game.js
+++ game.js
@@ -1,2 +1,7 @@
-var pseudo = "";
-var score = 0;
+function gameuhh() {
+  var pseudo = document.getElementById("pseudo").value;
+  sessionStorage.setItem("pseudo", pseudo);
+  document.location.href = "map.html";
+}
+console.log(sessionStorage.getItem("pseudo"));
+document.getElementById("blockn").append(sessionStorage.getItem("pseudo"));
