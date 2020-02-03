3544e956cff4bada3e2a038c99c87e18f992ca22
Victor Bour <victor.bour.bowden@gmail.com>
2020-02-03 23:17:05 +0100

    Ajout de l'affichage du score et du temps restant (ce dernier étant pas encore géré)

--- game.js
+++ game.js
@@ -4,4 +4,8 @@ function gameuhh() {
   document.location.href = "map.html";
 }
 console.log(sessionStorage.getItem("pseudo"));
-document.getElementById("blockn").append(sessionStorage.getItem("pseudo"));
+// document.getElementById("blockn").append(sessionStorage.getItem("pseudo"));
+
+$('#blockn').append($('<div>', {text: 'Score: ' + localStorage.getItem("score")}));
+$('#blockn').append($('<div>', {text: sessionStorage.getItem('pseudo')}));
+$('#blockn').append($('<div>', {text: 'Temps restant: '}));
