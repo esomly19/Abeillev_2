09de76303fda196a854ad4f53e7e36fa7e8c7dc0
Victor Bour <victor.bour.bowden@gmail.com>
2020-02-04 09:05:13 +0100

    Séparation du code pour la page de scan QR et de l'index (chargement du score)

---
+++ QRScanner.js
@@ -0,0 +1,61 @@
+import QrScanner from "./qr-scanner.min.js";
+
+QrScanner.WORKER_PATH = "./qr-scanner-worker.min.js";
+// Set constraints for the video stream
+const constraints = {
+  video: {
+    width: { max: 800 },
+    height: { max: 380 },
+    facingMode: "environment"
+  },
+  audio: false
+}; // Define constants
+const cameraView = document.querySelector("#camera--view"),
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger"); // Access the device camera and stream to cameraView
+function cameraStart() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+      const track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+      console.error("Oops. Something is broken.", error);
+    });
+}
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  window.location.href = "./map.html";
+}
+window.addEventListener(
+  "load",
+  () => {
+    cameraStart();
+
+    const qrScanner = new QrScanner(
+      document.getElementById("camera--view"),
+      result => {
+        qrScanner.destroy();
+        fetch("http://localhost:19080/fleurs/" + result).then(result => {
+          result.json().then(result => {
+            console.log(result);
+            score += result.nectar * result.pollen;
+            localStorage.setItem('score', score);
+            sessionStorage.setItem("score", score);
+            var x = setTimeout(showPage, 5000);
+            console.log(score);
+          });
+        });
+      }
+    );
+    qrScanner.start();
+  },
+  false
+);
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import QrScanner from "./qr-scanner.min.js";
 if(localStorage.getItem("score") != null){
   var score = Number(localStorage.getItem("score"));
 }
@@ -6,63 +5,3 @@ else {
   var score = 0;
   localStorage.setItem("score", score);
 }
-
-QrScanner.WORKER_PATH = "./qr-scanner-worker.min.js";
-// Set constraints for the video stream
-const constraints = {
-  video: {
-    width: { max: 800 },
-    height: { max: 380 },
-    facingMode: "environment"
-  },
-  audio: false
-}; // Define constants
-const cameraView = document.querySelector("#camera--view"),
-  cameraOutput = document.querySelector("#camera--output"),
-  cameraSensor = document.querySelector("#camera--sensor"),
-  cameraTrigger = document.querySelector("#camera--trigger"); // Access the device camera and stream to cameraView
-function cameraStart() {
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function(stream) {
-      const track = stream.getTracks()[0];
-      cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-      console.error("Oops. Something is broken.", error);
-    });
-}
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  window.location.href = "./map.html";
-}
-window.addEventListener(
-  "load",
-  () => {
-    cameraStart();
-
-    const qrScanner = new QrScanner(
-      document.getElementById("camera--view"),
-      result => {
-        qrScanner.destroy();
-        fetch("http://localhost:19080/fleurs/" + result).then(result => {
-          result.json().then(result => {
-            console.log(result);
-            score += result.nectar * result.pollen;
-            localStorage.setItem('score', score);
-            sessionStorage.setItem("score", score);
-            var x = setTimeout(showPage, 5000);
-            console.log(score);
-          });
-        });
-      }
-    );
-    qrScanner.start();
-  },
-  false
-);
