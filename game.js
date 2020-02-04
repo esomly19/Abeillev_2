eef2caecdd71afa10ed25d9c209a28ee0886dd67
Victor Bour <victor.bour.bowden@gmail.com>
2020-02-04 11:08:36 +0100

    Force l'utilisation d'un pseudonyme pour lancer la partie

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 function gameuhh() {
   var pseudo = document.getElementById("pseudo").value;
-  sessionStorage.setItem("pseudo", pseudo);
-  document.location.href = "map.html";
+  if(pseudo.length > 0){
+    sessionStorage.setItem("pseudo", pseudo);
+    document.location.href = "map.html";
+  }
+  else{
+    alert('Veuillez entrer un pseudonyme !');
+  }
+
 }
 console.log(sessionStorage.getItem("pseudo"));
 // document.getElementById("blockn").append(sessionStorage.getItem("pseudo"));
